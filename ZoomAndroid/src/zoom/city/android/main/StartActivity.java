d4635f3cc2d91efc3b984840a93225be64a9a3db
Andrija Karadžić <andrijak91@gmail.com>
2016-04-15 12:37:39 +0200

    commit1504
    Publish adjustments

--- ZoomAndroid/src/zoom/city/android/main/StartActivity.java
+++ ZoomAndroid/src/zoom/city/android/main/StartActivity.java
@@ -42,7 +42,7 @@ public class StartActivity extends Activity {
             for (Signature signature : info.signatures) {
                 MessageDigest md = MessageDigest.getInstance("SHA");
                 md.update(signature.toByteArray());
-                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+                Log.d("MYTAG", Base64.encodeToString(md.digest(), Base64.DEFAULT));
                 }
         } catch (NameNotFoundException e) {
         	Log.d("KeyHash:", "NameNitFound");
